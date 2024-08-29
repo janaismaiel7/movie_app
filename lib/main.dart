972000a9ef4b9a7cf617237d78564e53f8bf00cf
janaismaiel7 <janna.aismaiel@gmail.com>
2024-08-29 20:42:07 +0300

    added WatchList Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:movies_app/firebase_options.dart';
 import 'package:movies_app/first_screen/first_screen.dart';
 import 'package:movies_app/my_app_theme.dart';
 import 'package:movies_app/splashScreen/splashScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   await FirebaseFirestore.instance.enableNetwork();
   runApp(MyApp());
 }
 
--- lib/watchList_screen/watchList_screen.dart
+++ lib/watchList_screen/watchList_screen.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/model/popular_response.dart';
 import 'package:movies_app/my_app_colors.dart';
-import 'package:movies_app/watchList/watchlistIteam.dart';
+import 'package:movies_app/watchList_screen/WatchListIteam.dart';
+
 
 class WatchlistScreen extends StatelessWidget{
   @override
@@ -12,22 +13,30 @@ class WatchlistScreen extends StatelessWidget{
        backgroundColor: Colors.transparent,
       appBar: 
       AppBar(
+          backgroundColor: Colors.transparent,
         title: Text('Watch List',style: Theme.of(context).textTheme.titleLarge,),
       ),
-      body: StreamBuilder(stream: 
-      FirebaseFirestore.instance.collection('watchlist').snapshots(),
-       builder: (context,snapshot){
-        if(!snapshot.hasData){
-          return Center(child: CircularProgressIndicator(),);
-        }
-        final movies =snapshot.data!.docs.map((doc)=> Results.fromJson(doc.data())).toList();
-
-        return ListView.builder(
-          itemCount: movies.length,
-          itemBuilder: (context,index){
-            return Watchlistiteam( movie: movies[index]);
-          });
-       })
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder(stream: 
+        FirebaseFirestore.instance.collection('watchlist').snapshots(),
+         builder: (context,snapshot){
+          if(!snapshot.hasData){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          final movies =snapshot.data!.docs.map((doc)=> Results.fromJson(doc.data())).toList();
+        
+          return ListView.separated(
+        itemCount: movies.length,
+        separatorBuilder: (context, index) => Divider(
+          color: MyAppColors.greyColor,
+          thickness: 1.0,
+        ),
+        itemBuilder: (context, index) {
+          return Watchlistiteam(movie: movies[index]);
+            });
+         }),
+      )
     );
   }
 
